e5fa0f5c6396d219f0d660b2f19aec0e08721d56
Rana Das <rana.pratap.das@gmail.com>
2020-01-04 21:53:07 +0000

    Adding Api service barebones - step1

---
+++ rpt-01-api-service/src/main/java/com/rdas/api/ApiServiceMain.java
@@ -0,0 +1,11 @@
+package com.rdas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServiceMain.class, args);
+    }
+}
