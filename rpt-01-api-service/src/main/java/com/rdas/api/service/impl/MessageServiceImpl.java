bfc5403efa1c27c0c8dfd28359d30a4e72a90268
Rana Das <rana.pratap.das@gmail.com>
2020-01-05 16:18:52 +0000

    Adding all Queue names in yaml

--- rpt-01-api-service/src/main/java/com/rdas/api/service/impl/MessageServiceImpl.java
+++ rpt-01-api-service/src/main/java/com/rdas/api/service/impl/MessageServiceImpl.java
@@ -2,13 +2,14 @@ package com.rdas.api.service.impl;
 
 import com.rdas.api.service.MessageService;
 import com.rdas.common.message.model.ControlMessage;
+import com.rdas.common.message.observables.MessagePublisher;
 import com.rdas.common.message.observables.impl.ControlMessagePublisher;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageServiceImpl implements MessageService {
 
-    private final ControlMessagePublisher controlMessagePublisher;
+    private final MessagePublisher controlMessagePublisher;
 
     public MessageServiceImpl(ControlMessagePublisher controlMessagePublisher) {
         this.controlMessagePublisher = controlMessagePublisher;
