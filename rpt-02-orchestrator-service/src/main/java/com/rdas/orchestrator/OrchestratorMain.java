e9c8332f067626022bf839c76b0b0ec169d79e3d
Rana Das <rana.pratap.das@gmail.com>
2020-01-05 16:20:23 +0000

    Adding Orchestrator service with subscription to Control Messages

---
+++ rpt-02-orchestrator-service/src/main/java/com/rdas/orchestrator/OrchestratorMain.java
@@ -0,0 +1,11 @@
+package com.rdas.orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorMain {
+    public static void main(String[] args) {
+        SpringApplication.run(OrchestratorMain.class, args);
+    }
+}
