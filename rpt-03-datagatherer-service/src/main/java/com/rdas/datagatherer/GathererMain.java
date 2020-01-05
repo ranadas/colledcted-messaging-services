2486ade2a6146dfb7150b77ca3bcf33960ff51fe
Rana Das <rana.pratap.das@gmail.com>
2020-01-05 20:11:31 +0000

    Adding datagatherer service scaffolding

---
+++ rpt-03-datagatherer-service/src/main/java/com/rdas/datagatherer/GathererMain.java
@@ -0,0 +1,11 @@
+package com.rdas.datagatherer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GathererMain {
+    public static void main(String[] args) {
+        SpringApplication.run(GathererMain.class, args);
+    }
+}
